2f718479b30f3d499f57f8c4143775b6c5cf130e
mishoto <mihail.dev.io@gmail.com>
2023-02-14 09:52:54 +0200

    change logic to return password from request

--- src/main/java/org/example/controllers/UserController.java
+++ src/main/java/org/example/controllers/UserController.java
@@ -6,12 +6,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.models.entities.UserEntity;
 import org.example.models.requests.ChangeUserAccessRequest;
-import org.example.models.requests.ChangeUserRoleRequest;
 import org.example.models.requests.LoginUserRequest;
 import org.example.models.requests.RegisterUserRequest;
 import org.example.models.responses.*;
 import org.example.services.UserService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,8 +33,15 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    Optional<UserEntity> login(@RequestBody LoginUserRequest request) {
-        return userService.findUserByUsername(request.getUsername());
+    ResponseEntity<LoginUserResponse> login(@RequestBody LoginUserRequest request) {
+        Optional<UserEntity> userEntity = userService.findUserByUsername(request.getUsername());
+        if(userEntity.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        LoginUserResponse loginUserResponse = new LoginUserResponse(
+                request.getUsername(), request.getPassword(), userEntity.get().getRole().name()
+        );
+        return new ResponseEntity<>(loginUserResponse, HttpStatus.OK);
     }
 
     @PreAuthorize("hasAnyRole('ADMINISTRATOR', 'SUPPORT')")
@@ -54,12 +61,6 @@ public class UserController {
     ChangeUserAccessResponse access(@RequestBody ChangeUserAccessRequest request) {
         ChangeUserAccessResponse response = userService.changeAccess(request.getUsername(), request.getOperation());
         return new ChangeUserAccessResponse(response.getStatus());
-    }
-
-    @PreAuthorize("hasRole('ADMINISTRATOR')")
-    @PutMapping("/role")
-    ChangeUserRoleResponse role(@RequestBody ChangeUserRoleRequest request) {
-        return userService.changeRole(request.getUsername(), request.getRole());
     }
 }
 
