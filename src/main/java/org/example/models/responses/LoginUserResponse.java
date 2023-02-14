e4c7487a4425351a0e682747ab5256b936d00e68
mishoto <mihail.dev.io@gmail.com>
2023-02-14 08:44:50 +0200

    add LoginUserResponse

---
+++ src/main/java/org/example/models/responses/LoginUserResponse.java
@@ -0,0 +1,16 @@
+package org.example.models.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginUserResponse {
+    String username;
+    String password;
+    String role;
+}
